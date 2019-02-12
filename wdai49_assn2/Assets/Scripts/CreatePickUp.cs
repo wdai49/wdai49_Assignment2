e0dbbad1f5daa9bfc40a6fb1063a99025ada87ba
wdai49 <46665590+wdai49@users.noreply.github.com>
2019-02-11 21:18:37 -0500

    Initial commit

---
+++ wdai49_assn2/Assets/Scripts/CreatePickUp.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatePickUp : MonoBehaviour
+{
+    public GameObject prefeb;
+    GameObject pickup;
+    float randomNumber;
+    float posX, posZ, angle;
+    public float radius;
+    public int numPickup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < numPickup; i++)
+        {
+            angle = i * 2f * Mathf.PI / numPickup;
+            randomNumber = Random.value;
+            posX = Mathf.Cos(angle) * radius;
+            posZ = Mathf.Sin(angle) * radius;
+
+
+            pickup = Instantiate<GameObject>(prefeb);
+            if (randomNumber < 0.3f)
+            {
+                Renderer rend = pickup.GetComponent<Renderer>();
+                rend.material.color = Color.green;
+                pickup.tag = "green";
+            }
+            else if (randomNumber < 0.6f)
+            {
+                pickup.GetComponent<Renderer>().material.color = Color.magenta;
+                pickup.tag = "magenta";
+            }
+            else
+            {
+                pickup.GetComponent<Renderer>().material.color = Color.red;
+                pickup.tag = "red";
+            }
+
+            pickup.transform.position = new Vector3(posX, 0.5f, posZ);
+        }
+    }
+
+}
---
+++ wdai49_assn2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+    public Text scoreText;
+    int score;
+    int point;
+    bool isDetected;
+
+    public int Point
+    {
+        set { point = value; }
+        get { return point; }
+    }
+  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        score = 0;
+        SetScoreText();
+    }
+
+
+     void FixedUpdate()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement*speed);
+
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        isDetected = false;
+        if (collision.gameObject.tag == "green")
+        {
+            Point= 1;
+            isDetected = true;
+        }
+        else if (collision.gameObject.tag == "magenta")
+        {
+            Point= 10;
+            isDetected = true;
+        }
+        else if(collision.gameObject.tag=="red")
+        {
+            Point= 100;
+            isDetected = true;
+        }
+
+        if (isDetected)
+        {
+            score += Point;
+            Destroy(collision.gameObject);
+            SetScoreText();
+        }
+    }
+
+    void SetScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
+}
